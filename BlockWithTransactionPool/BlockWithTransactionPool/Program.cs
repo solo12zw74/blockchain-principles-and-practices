6bed9be9ffb4aafd6ed3a3208faacc14eb34fefb
solo12zw74 <solo12zw74@gmail.com>
2023-02-25 15:34:14 -0600

    Butify Program.cs
    Wrap pieces of the file into logical methods

--- BlockWithTransactionPool/BlockWithTransactionPool/Program.cs
+++ BlockWithTransactionPool/BlockWithTransactionPool/Program.cs
@@ -2,67 +2,97 @@
 
 using BlockWithTransactionPool;
 
-ITransaction txn1 = new Transaction("ABC123", 1000.00m, DateTime.Now, "QWE123", 10000, ClaimType.TotalLoss);
-ITransaction txn2 = new Transaction("VBG345", 2000.00m, DateTime.Now, "JKH567", 20000, ClaimType.TotalLoss);
-ITransaction txn3 = new Transaction("XCF234", 3000.00m, DateTime.Now, "DH23ED", 30000, ClaimType.TotalLoss);
-ITransaction txn4 = new Transaction("CBHD45", 4000.00m, DateTime.Now, "DH34K6", 40000, ClaimType.TotalLoss);
-ITransaction txn5 = new Transaction("AJD345", 5000.00m, DateTime.Now, "28FNF4", 50000, ClaimType.TotalLoss);
-ITransaction txn6 = new Transaction("QAX367", 6000.00m, DateTime.Now, "FJK676", 60000, ClaimType.TotalLoss);
-ITransaction txn7 = new Transaction("CGO444", 7000.00m, DateTime.Now, "LKU234", 70000, ClaimType.TotalLoss);
-ITransaction txn8 = new Transaction("PLO254", 8000.00m, DateTime.Now, "VBN456", 80000, ClaimType.TotalLoss);
-ITransaction txn9 = new Transaction("ABC123", 1000.00m, DateTime.Now, "QWE123", 10000, ClaimType.TotalLoss);
-ITransaction txn10 = new Transaction("VBG345", 2000.00m, DateTime.Now, "JKH567", 20000, ClaimType.TotalLoss);
-ITransaction txn11 = new Transaction("XCF234", 3000.00m, DateTime.Now, "DH23ED", 30000, ClaimType.TotalLoss);
-ITransaction txn12 = new Transaction("CBHD45", 4000.00m, DateTime.Now, "DH34K6", 40000, ClaimType.TotalLoss);
-ITransaction txn13 = new Transaction("AJD345", 5000.00m, DateTime.Now, "28FNF4", 50000, ClaimType.TotalLoss);
-ITransaction txn14 = new Transaction("QAX367", 6000.00m, DateTime.Now, "FJK676", 60000, ClaimType.TotalLoss);
-ITransaction txn15 = new Transaction("CGO444", 7000.00m, DateTime.Now, "LKU234", 70000, ClaimType.TotalLoss);
-ITransaction txn16 = new Transaction("PLO254", 8000.00m, DateTime.Now, "VBN456", 80000, ClaimType.TotalLoss);
+var txnPool = new TransactionPool();
 
-IBlock block1 = new Block(0, null);
-IBlock block2 = new Block(1, null);
-IBlock block3 = new Block(2, null);
-IBlock block4 = new Block(3, null);
+var txnToBeChanged = SetupTransactions(txnPool);
 
-block1.AddTransaction(txn1);
-block1.AddTransaction(txn2);
-block1.AddTransaction(txn3);
-block1.AddTransaction(txn4);
+var keyStore = new KeyStore(Hmac.GenerateKey());
 
-block2.AddTransaction(txn5);
-block2.AddTransaction(txn6);
-block2.AddTransaction(txn7);
-block2.AddTransaction(txn8);
+IBlock block1 = new Block(0, keyStore);
+IBlock block2 = new Block(1, keyStore);
+IBlock block3 = new Block(2, keyStore);
+IBlock block4 = new Block(3, keyStore);
 
-block3.AddTransaction(txn9);
-block3.AddTransaction(txn10);
-block3.AddTransaction(txn11);
-block3.AddTransaction(txn12);
+FillBlocks(block1, block2, block3, block4);
 
-block4.AddTransaction(txn13);
-block4.AddTransaction(txn14);
-block4.AddTransaction(txn15);
-block4.AddTransaction(txn16);
-
-block1.SetBlockHash(null);
-block2.SetBlockHash(block1);
-block3.SetBlockHash(block2);
-block4.SetBlockHash(block3);
-
-
-BlockChain chain = new BlockChain();
+var chain = new BlockChain();
 chain.AcceptBlock(block1);
 chain.AcceptBlock(block2);
 chain.AcceptBlock(block3);
 chain.AcceptBlock(block4);
 
-
 chain.VerifyChain();
 
 Console.WriteLine("");
 Console.WriteLine("");
 
-txn5.Mileage = 12345;
+txnToBeChanged.Mileage = 12345;
 chain.VerifyChain();
 
 Console.WriteLine();
+
+ITransaction SetupTransactions(TransactionPool transactionPool)
+{
+    ITransaction txn1 = new Transaction("ABC123", 1000.00m, DateTime.Now, "QWE123", 10000, ClaimType.TotalLoss);
+    ITransaction txn2 = new Transaction("VBG345", 2000.00m, DateTime.Now, "JKH567", 20000, ClaimType.TotalLoss);
+    ITransaction txn3 = new Transaction("XCF234", 3000.00m, DateTime.Now, "DH23ED", 30000, ClaimType.TotalLoss);
+    ITransaction txn4 = new Transaction("CBHD45", 4000.00m, DateTime.Now, "DH34K6", 40000, ClaimType.TotalLoss);
+    ITransaction txn5 = new Transaction("AJD345", 5000.00m, DateTime.Now, "28FNF4", 50000, ClaimType.TotalLoss);
+    ITransaction txn6 = new Transaction("QAX367", 6000.00m, DateTime.Now, "FJK676", 60000, ClaimType.TotalLoss);
+    ITransaction txn7 = new Transaction("CGO444", 7000.00m, DateTime.Now, "LKU234", 70000, ClaimType.TotalLoss);
+    ITransaction txn8 = new Transaction("PLO254", 8000.00m, DateTime.Now, "VBN456", 80000, ClaimType.TotalLoss);
+    ITransaction txn9 = new Transaction("ABC123", 1000.00m, DateTime.Now, "QWE123", 10000, ClaimType.TotalLoss);
+    ITransaction txn10 = new Transaction("VBG345", 2000.00m, DateTime.Now, "JKH567", 20000, ClaimType.TotalLoss);
+    ITransaction txn11 = new Transaction("XCF234", 3000.00m, DateTime.Now, "DH23ED", 30000, ClaimType.TotalLoss);
+    ITransaction txn12 = new Transaction("CBHD45", 4000.00m, DateTime.Now, "DH34K6", 40000, ClaimType.TotalLoss);
+    ITransaction txn13 = new Transaction("AJD345", 5000.00m, DateTime.Now, "28FNF4", 50000, ClaimType.TotalLoss);
+    ITransaction txn14 = new Transaction("QAX367", 6000.00m, DateTime.Now, "FJK676", 60000, ClaimType.TotalLoss);
+    ITransaction txn15 = new Transaction("CGO444", 7000.00m, DateTime.Now, "LKU234", 70000, ClaimType.TotalLoss);
+    ITransaction txn16 = new Transaction("PLO254", 8000.00m, DateTime.Now, "VBN456", 80000, ClaimType.TotalLoss);
+
+    transactionPool.AddTransaction(txn1);
+    transactionPool.AddTransaction(txn2);
+    transactionPool.AddTransaction(txn3);
+    transactionPool.AddTransaction(txn4);
+    transactionPool.AddTransaction(txn5);
+    transactionPool.AddTransaction(txn6);
+    transactionPool.AddTransaction(txn7);
+    transactionPool.AddTransaction(txn8);
+    transactionPool.AddTransaction(txn9);
+    transactionPool.AddTransaction(txn10);
+    transactionPool.AddTransaction(txn11);
+    transactionPool.AddTransaction(txn12);
+    transactionPool.AddTransaction(txn13);
+    transactionPool.AddTransaction(txn14);
+    transactionPool.AddTransaction(txn15);
+    transactionPool.AddTransaction(txn16);
+    return txn5;
+}
+
+void FillBlocks(IBlock block, IBlock block5, IBlock block6, IBlock block7)
+{
+    block.AddTransaction(txnPool.GetTransaction());
+    block.AddTransaction(txnPool.GetTransaction());
+    block.AddTransaction(txnPool.GetTransaction());
+    block.AddTransaction(txnPool.GetTransaction());
+
+    block5.AddTransaction(txnPool.GetTransaction());
+    block5.AddTransaction(txnPool.GetTransaction());
+    block5.AddTransaction(txnPool.GetTransaction());
+    block5.AddTransaction(txnPool.GetTransaction());
+
+    block6.AddTransaction(txnPool.GetTransaction());
+    block6.AddTransaction(txnPool.GetTransaction());
+    block6.AddTransaction(txnPool.GetTransaction());
+    block6.AddTransaction(txnPool.GetTransaction());
+
+    block7.AddTransaction(txnPool.GetTransaction());
+    block7.AddTransaction(txnPool.GetTransaction());
+    block7.AddTransaction(txnPool.GetTransaction());
+    block7.AddTransaction(txnPool.GetTransaction());
+
+    block.SetBlockHash(null);
+    block5.SetBlockHash(block);
+    block6.SetBlockHash(block5);
+    block7.SetBlockHash(block6);
+}
